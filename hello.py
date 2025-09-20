d5ba0e6fa8d3643f5805741e7bb08ec72862a0b2
Richelle Pereira <richelle0001@gmail.com>
2025-09-19 21:58:03 -0400

    Reproduced example 2-2 in previous hello.py code - added dynamic routing.

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
-# hello.py - Example 2.1 
-
 from flask import Flask
 app = Flask(__name__)
 
+# hello.py - Example 2.1 - Simple Route
 @app.route('/')
 def index():
     return '<h1>Hello, World!</h1>'
+
+# hello.py - Example 2.2 - Dynamic Routing
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
