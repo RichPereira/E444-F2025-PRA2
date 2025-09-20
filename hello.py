5e57a64fea628ca1b5fd3fa86ba7623c98c127f4
Richelle Pereira <richelle0001@gmail.com>
2025-09-19 22:52:38 -0400

    Reproduced screenshot in activity 1.3 and added js script to dynamically update time ago.

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 
 # hello.py - Example 2.1 - Simple Route
 @app.route('/')
 def index():
-    return '<h1>Hello, World!</h1>'
+    return render_template('index.html')
 
 # hello.py - Example 2.2 - Dynamic Routing
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name, timestamp=datetime.now())
+
+# hello.py - Example 2.3 - Using Flask-Bootstrap
+Bootstrap = Bootstrap(app)
